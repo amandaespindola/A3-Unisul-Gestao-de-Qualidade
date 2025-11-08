8b0e0527d30a6c0217be5b7e441249c6713f4cf8
laurafoes <foeslaura@gmail.com>
2025-11-08 10:27:40 -0300

    refactor: refatorados métodos utilizados para criação de aluno e adicionada classe Constantes

---
+++ src/main/java/utils/Constantes.java
@@ -0,0 +1,24 @@
+package utils;
+
+public final class Constantes {
+
+    private Constantes() {
+        throw new UnsupportedOperationException("Classe de constantes não pode ser instanciada");
+    }
+
+    public static final String[] CURSOS = {
+        "-",
+        "Administração",
+        "Análise e Desenvolvimento de Sistemas",
+        "Arquitetura e Urbanismo",
+        "Ciências Contábeis",
+        "Ciências da Computação",
+        "Design",
+        "Design de Moda",
+        "Relações Internacionais",
+        "Sistemas de Informação"
+    };
+
+    public static final int[] FASES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+}
