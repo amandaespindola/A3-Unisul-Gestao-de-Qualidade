30bb30f065629a2710e2f439d0de81d9b752e9a7
Amanda <amandaespindola2004@gmail.com>
2025-11-11 13:01:59 -0300

    refactor: adiciona cosntrutor privado à classe interna UIConstants para evitar intanciação

--- src/main/java/utils/Constantes.java
+++ src/main/java/utils/Constantes.java
@@ -2,44 +2,26 @@ package utils;
 
 public final class Constantes {
 
-    private Constantes() {
-        throw new UnsupportedOperationException("Classe de constantes não pode ser instanciada");
-    }
+	private Constantes() {
+		throw new UnsupportedOperationException("Classe de constantes não pode ser instanciada");
+	}
 
-    public static final String[] CURSOS = {
-        "-",
-        "Administração",
-        "Análise e Desenvolvimento de Sistemas",
-        "Arquitetura e Urbanismo",
-        "Ciências Contábeis",
-        "Ciências da Computação",
-        "Design",
-        "Design de Moda",
-        "Relações Internacionais",
-        "Sistemas de Informação"
-    };
+	public static final String[] CURSOS = { "-", "Administração", "Análise e Desenvolvimento de Sistemas",
+			"Arquitetura e Urbanismo", "Ciências Contábeis", "Ciências da Computação", "Design", "Design de Moda",
+			"Relações Internacionais", "Sistemas de Informação" };
 
-    public static final int[] FASES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	public static final int[] FASES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    public static final String[] CAMPUS = {
-        "-",
-        "Continente",
-        "Dib Mussi",
-        "Ilha",
-        "Pedra Branca",
-        "Trajano",
-        "Tubarão"
-    };
+	public static final String[] CAMPUS = { "-", "Continente", "Dib Mussi", "Ilha", "Pedra Branca", "Trajano",
+			"Tubarão" };
 
-    public static final String[] TITULOS = {
-        "-",
-        "Graduação",
-        "Especialização",
-        "Mestrado",
-        "Doutorado"
-    };
+	public static final String[] TITULOS = { "-", "Graduação", "Especialização", "Mestrado", "Doutorado" };
 
-    public class UIConstants {
-    	public static final String DEFAULT_FONT = "Segoe UI";
-    }
+	public class UIConstants {
+		private UIConstants() {
+			throw new UnsupportedOperationException("Classe de constantes UI não pode ser instanciada");
+		}
+
+		public static final String DEFAULT_FONT = "Segoe UI";
+	}
 }
