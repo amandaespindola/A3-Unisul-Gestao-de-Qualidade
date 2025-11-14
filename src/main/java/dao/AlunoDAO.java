95bfefb2c55092541ec649b5d724d80acc017269
Amanda <amandaespindola2004@gmail.com>
2025-11-13 21:01:41 -0300

    fix: remove import * para utilizar imports especificos

--- src/main/java/dao/AlunoDAO.java
+++ src/main/java/dao/AlunoDAO.java
@@ -1,12 +1,17 @@
 package dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import model.Aluno;
 import utils.DaoUtils;
-import java.util.List;
 
 public class AlunoDAO extends BaseDAO<Aluno> {
 
