2b558aaf434503da034d0d68fb3d2f03857e033e
Matheus Bernardo <matheus008bernardo@gmail.com>
2025-11-14 16:35:47 -0300

    docs: adiciona javadoc completo em ProfessorBase

--- src/main/java/model/ProfessorDTO.java
+++ src/main/java/model/ProfessorDTO.java
@@ -1,5 +1,15 @@
 package model;
 
+/**
+ * Data Transfer Object (DTO) para representar dados de um professor.
+ * <p>
+ * Esta classe herda todos os atributos de {@link ProfessorBase} e é utilizada
+ * exclusivamente para transporte de informações entre camadas da aplicação
+ * (como interface gráfica, controladores e a camada de persistência).
+ * </p>
+ *
+ * <p>Nenhuma regra de negócio deve ser implementada nesta classe.</p>
+ */
 public class ProfessorDTO extends ProfessorBase{
 
 }
