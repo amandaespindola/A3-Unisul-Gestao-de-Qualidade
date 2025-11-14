11a48119488e2eaea4bdaf79a8a660cfd5731de0
Matheus Bernardo <matheus008bernardo@gmail.com>
2025-11-14 18:17:35 -0300

    docs: adiciona javadoc completo em LookAndFeelHelper

--- src/main/java/utils/LookAndFeelHelper.java
+++ src/main/java/utils/LookAndFeelHelper.java
@@ -2,12 +2,27 @@ package utils;
 
 import javax.swing.UIManager;
 
+/**
+ * Utilitário para aplicação do tema Nimbus no Swing.
+ * <p>
+ * Esta classe fornece um método estático que tenta aplicar o Look and Feel
+ * Nimbus, caso esteja disponível no sistema. Não pode ser instanciada.
+ */
 public final class LookAndFeelHelper {
 
+        /**
+        * Construtor privado para impedir instanciação.
+        */
 	private LookAndFeelHelper() {
 		// Impede instanciação
 	}
 
+        /**
+        * Aplica o Look and Feel Nimbus, caso esteja instalado.
+        * <p>
+        * Se o Nimbus não estiver disponível ou ocorrer alguma falha na aplicação
+        * do tema, a exceção é capturada silenciosamente.
+        */
 	public static void aplicarNimbus() {
 		try {
 			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
