b97e8bc22324f21631f3e884d36d2966f2659911
Matheus Bernardo <matheus008bernardo@gmail.com>
2025-11-14 16:32:09 -0300

    docs: adiciona javadoc completo em ProfessorBase

--- src/main/java/model/ProfessorBase.java
+++ src/main/java/model/ProfessorBase.java
@@ -1,16 +1,45 @@
 package model;
 
+/**
+ * Classe base para representar informações comuns de um professor.
+ * <p>
+ * Esta classe estende {@link Pessoa} e adiciona atributos específicos relacionados
+ * ao vínculo e formação do professor, como campus, CPF, contato, título acadêmico
+ * e salário.
+ * </p>
+ */ 
 public class ProfessorBase extends Pessoa {
+        
+        /** Campus onde o professor atua. */
 	protected String campus;
+        
+        /** CPF do professor. */
 	protected String cpf;
+        
+        /** Informações de contato do professor. */
 	protected String contato;
+        
+        /** Título acadêmico do professor (ex.: Mestre, Doutor). */
 	protected String titulo;
+        
+        /** Salário atual do professor. */
 	protected double salario;
 
+        /**
+        * Construtor padrão.
+        * Inicializa a classe com valores padrão de {@link Pessoa}.
+        */
 	public ProfessorBase() {
 		super();
 	}
 
+        /**
+        * Construtor que inicializa os dados herdados de {@link Pessoa}.
+        *
+        * @param id    Identificador único do professor.
+        * @param nome  Nome do professor.
+        * @param idade Idade do professor.
+        */
 	public ProfessorBase(int id, String nome, int idade) {
 		super(id, nome, idade);
 	}
