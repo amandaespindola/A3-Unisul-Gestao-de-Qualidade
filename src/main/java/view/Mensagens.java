2658a96d7dbd7fa42d9a7cde0cebcf331c9d8cae
Gustavoaals <gustavoalvess842@gmail.com>
2025-11-15 08:14:00 -0300

    Adicionado Javadoc na classe Mensagens

--- src/main/java/view/Mensagens.java
+++ src/main/java/view/Mensagens.java
@@ -1,7 +1,16 @@
 package view;
 
+/**
+ * Classe de exceção personalizada para mensagens específicas do sistema.
+ * Estende a classe {@link Exception}.
+ */
 public class Mensagens extends Exception {
 
+    /**
+     * Construtor que recebe uma mensagem específica para a exceção.
+     *
+     * @param msg A mensagem da exceção.
+     */
     public Mensagens(String msg) {
         super(msg);
     }
