713a0299136de8e32bc30a08c1d2299ed1c61a94
Gustavoaals <gustavoalvess842@gmail.com>
2025-11-14 23:22:53 -0300

    Adicionado Javadoc na classe Aluno

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -4,24 +4,57 @@ import java.util.List;
 
 import dao.AlunoDAO;
 
-
+/**
+ * Classe que representa um Aluno no sistema, herdando de Pessoa.
+ * Contém atributos específicos (curso, fase) e métodos de interação com o banco.
+ */
 public class Aluno extends Pessoa {
 
     // Atributos
+
+    /**
+     * Curso que o aluno está cursando.
+     */
     private String curso;
+
+    /**
+     * Fase (período/semestre) atual do aluno.
+     */
     private int fase;
+
+    /**
+     * Objeto responsável pela comunicação com o banco de dados.
+     */
     private final AlunoDAO dao;
 
+    /**
+     * Construtor padrão que inicializa o DAO.
+     */
     public Aluno() {
         this.dao = new AlunoDAO();
     }
 
+    /**
+     * Construtor que recebe curso e fase.
+     *
+     * @param curso curso do aluno
+     * @param fase fase do aluno
+     */
     public Aluno(String curso, int fase) {
         this.curso = curso;
         this.fase = fase;
         this.dao = new AlunoDAO();
     }
 
+    /**
+     * Construtor completo que recebe também os atributos da classe Pessoa.
+     *
+     * @param curso curso do aluno
+     * @param fase fase atual do aluno
+     * @param id id do aluno
+     * @param nome nome do aluno
+     * @param idade idade do aluno
+     */
     public Aluno(String curso, int fase, int id, String nome, int idade) {
         super(id, nome, idade);
         this.curso = curso;
@@ -30,22 +63,48 @@ public class Aluno extends Pessoa {
     }
 
     // Métodos GET e SET
+
+    /**
+     * Retorna o curso do aluno.
+     *
+     * @return curso do aluno
+     */
     public String getCurso() {
         return curso;
     }
 
+    /**
+     * Define o curso do aluno.
+     *
+     * @param curso novo curso
+     */
     public void setCurso(String curso) {
         this.curso = curso;
     }
 
+    /**
+     * Retorna a fase do aluno.
+     *
+     * @return fase do aluno
+     */
     public int getFase() {
         return fase;
     }
 
+    /**
+     * Altera a fase do aluno.
+     *
+     * @param fase nova fase
+     */
     public void setFase(int fase) {
         this.fase = fase;
     }
 
+    /**
+     * Retorna uma representação em texto do aluno.
+     *
+     * @return dados formatados do aluno
+     */
     @Override
     public String toString() {
         return "\n ID: " + this.getId()
@@ -56,38 +115,72 @@ public class Aluno extends Pessoa {
                 + "\n -----------";
     }
 
-  
-    // Retorna a Lista de Alunos (objetos)
+    /**
+     * Retorna a lista de todos os alunos cadastrados.
+     *
+     * @return lista de alunos
+     */
     public List<Aluno> getMinhaLista() {
         return dao.getMinhaLista();
     }
 
-    // Cadastra novo aluno
+    /**
+     * Insere um novo aluno no banco de dados.
+     *
+     * @param curso curso do aluno
+     * @param fase fase do aluno
+     * @param nome nome do aluno
+     * @param idade idade do aluno
+     * @return true se inserido com sucesso
+     */
     public boolean inserirAlunoBd(String curso, int fase, String nome, int idade) {
         Aluno objeto = new Aluno(curso, fase, 0, nome, idade);
         return dao.insert(objeto);
     }
 
-    // Deleta um aluno específico pelo seu campo ID
+    /**
+     * Deleta um aluno do banco através do ID.
+     *
+     * @param id identificador do aluno
+     * @return true após realizar a operação
+     */
     public boolean deletarAlunoBD(int id) {
         dao.delete(id);
         return true;
     }
 
-    // Edita um aluno específico pelo seu campo ID
+    /**
+     * Atualiza os dados de um aluno existente.
+     *
+     * @param curso novo curso
+     * @param fase nova fase
+     * @param id id do aluno
+     * @param nome novo nome
+     * @param idade nova idade
+     * @return true após realizar a operação
+     */
     public boolean atualizarAlunoBD(String curso, int fase, int id, String nome, int idade) {
         Aluno objeto = new Aluno(curso, fase, id, nome, idade);
         dao.update(objeto);
         return true;
     }
 
-    // carrega dados de um aluno específico pelo seu ID
+    /**
+     * Carrega os dados de um aluno específico pelo ID.
+     *
+     * @param id identificador do aluno
+     * @return objeto Aluno encontrado ou null
+     */
     public Aluno carregaAluno(int id) {
         dao.findById(id);
         return null;
     }
 
-    // retorna o maior ID da nossa base de dados
+    /**
+     * Retorna o maior ID registrado no banco de dados.
+     *
+     * @return maior ID encontrado
+     */
     public int obterMaiorId() {
         return dao.obterMaiorId();
     }
