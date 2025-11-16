8ebf4ae27c8c834607f0b98d9c8056ca9d2822ec
laurafoes <foeslaura@gmail.com>
2025-11-16 10:11:46 -0300

    test: adiciona testes headless safe para LookAndFeelHelper

---
+++ src/test/java/utils/LookAndFeelHelperTest.java
@@ -0,0 +1,46 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.swing.UIManager;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LookAndFeelHelperTest {
+
+    @Test
+    @DisplayName("aplicarNimbus não deve lançar exceções")
+    void testAplicarNimbusNaoLanca() {
+        assertDoesNotThrow(() -> LookAndFeelHelper.aplicarNimbus());
+    }
+
+    @Test
+    @DisplayName("aplicarNimbus deve manter um LookAndFeel válido")
+    void testLookAndFeelValido() {
+        assertDoesNotThrow(() -> LookAndFeelHelper.aplicarNimbus());
+
+        String laf = UIManager.getLookAndFeel().getName();
+
+        assertNotNull(laf);
+        assertFalse(laf.isBlank());
+    }
+
+    @Test
+    @DisplayName("Construtor privado deve existir e ser privado")
+    void testConstructorPrivate() throws Exception {
+        var constructor = LookAndFeelHelper.class.getDeclaredConstructor();
+
+        // Deve ser private
+        assertFalse(constructor.canAccess(null));
+
+        constructor.setAccessible(true);
+
+        Object instance = constructor.newInstance();
+        assertNotNull(instance);
+        assertEquals(LookAndFeelHelper.class, instance.getClass());
+    }
+}
